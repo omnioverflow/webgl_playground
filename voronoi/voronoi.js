a7aa70cd0091c327147403626d81f9305bd8f14c
iirthw <iirthw@gmail.com>
2021-04-25 23:25:40 +0300

    [style] empty line

--- voronoi/voronoi.js
+++ voronoi/voronoi.js
@@ -17,7 +17,6 @@ window.onload = function init()
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-
     // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader")
     gl.useProgram(program);
