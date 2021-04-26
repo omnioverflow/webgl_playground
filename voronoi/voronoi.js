4c122a2b5aa28c5ca31d0a569410f26e175ecdc4
iirthw <iirthw@gmail.com>
2021-04-26 23:12:41 +0300

    minor refactoring

--- voronoi/voronoi.js
+++ voronoi/voronoi.js
@@ -1,5 +1,7 @@
 var gl;
 var points;
+var program;
+var vao;
 
 window.onload = function init()
 {
@@ -11,36 +13,47 @@ window.onload = function init()
     }
 
     const vertexData = new Float32Array([-1, -1, 0, 1, 1, -1]);
-    const colors = new Float32Array([1.0, 0.0, 0.0, 1.0, 1.0, 1.0]);
 
     // Configure WebGL
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     // Load shaders and initialize attribute buffers
-    const program = initShaders(gl, "vertex-shader", "fragment-shader")
-    gl.useProgram(program);
+    program = initShaders(gl, "vertex-shader", "fragment-shader");    
 
     // Load the data into the GPU
     const bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+    
+    {
+        const pos = gl.getAttribLocation(program, "iPosition");
+        const size = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0; // == 0 -- move forward size * sizeof(type) to get the next position
+        const offset = 0; // start at the beginning of the buffer 
+        gl.vertexAttribPointer(pos, size, type, normalize, stride, offset);
+        gl.enableVertexAttribArray(pos);
+    }
 
-    // Associate out shader variables with our data buffer
-    const iPosition = gl.getAttribLocation(program, "iPosition");
-    gl.vertexAttribPointer(iPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(iPosition);
-
-    const iColor = gl.getAttribLocation(program, "iColor");
-    gl.vertexAttribPointer(iColor, 2 /* size */, gl.FLOAT /* type */, 
-        false /*normalized*/, 0 /* stride */, 0 /* pointer */);
-    gl.enableVertexAttribArray(iColor);
+    {
+        const pos = gl.getAttribLocation(program, "iColor");
+        const size = 2;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.vertexAttribPointer(pos, size, type, normalize, stride, offset);
+        gl.enableVertexAttribArray(pos);
+    }
 
     render();
 };
 
 function render()
 {
+    gl.useProgram(program);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
