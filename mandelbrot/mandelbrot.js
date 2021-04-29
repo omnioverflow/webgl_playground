ba5b0ee50bb571a39e481424d43fafa4f359e5a3
iirthw <iirthw@gmail.com>
2021-04-29 21:32:37 +0300

    use float literals for the vertex data

--- mandelbrot/mandelbrot.js
+++ mandelbrot/mandelbrot.js
@@ -14,9 +14,14 @@ window.onload = function init()
     }
 
     const vertexData = new Float32Array([
-        -1, -1, 0, 1, 1, -1, // positions
-        0.0, 0.0, 0.1, 0.0, 1.0, 0.2, 0.0, 0.5, 0.5 // colors
+        // positions
+        -1.0, -1.0, /* bottom left */ 0.0, 0.0, 0.1,
+        -1.0, 1.0, /* top left*/ 0.0, 1.0, 0.2,
+         1.0, -1.0, /* bottom right */ 0.0, 0.5, 0.5,
+         1.0, 1.0, /* top right */ 0.1, 0.3, 0.0     
         ]);
+
+    const indices = new Uint16Array([0, 1, 2, 1, 3, 2]);
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
@@ -32,13 +37,20 @@ window.onload = function init()
 
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+    {
+        const indicesBufferId = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBufferId);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    }
     
+    const sizeOfFloat = 4; // sizeof float in webgl
     {
         const pos = gl.getAttribLocation(program, "iPosition");
         const size = 2;
         const type = gl.FLOAT;
         const normalize = false;
-        const stride = 0; // == 0 -- move forward size * sizeof(type) to get the next position
+        const stride = 5 * sizeOfFloat; // == 0 -- move forward size * sizeof(type) to get the next position
         const offset = 0; // start at the beginning of the buffer 
         gl.vertexAttribPointer(pos, size, type, normalize, stride, offset);
         gl.enableVertexAttribArray(0);
@@ -49,9 +61,9 @@ window.onload = function init()
         const size = 3;
         const type = gl.FLOAT;
         const normalize = false;
-        const stride = 0;
-        const sizeOfType = 4; // sizeof float in webgl
-        const offset = 6 * sizeOfType;
+        
+        const stride = 5 * sizeOfFloat;
+        const offset = 3 * sizeOfFloat;
         gl.vertexAttribPointer(pos, size, type, normalize, stride, offset);
         gl.enableVertexAttribArray(1);
     }
@@ -66,5 +78,10 @@ function render()
     gl.useProgram(program);
     gl.bindVertexArray(vao);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    {
+        const vertexCount = 6;
+        const type = gl.UNSIGNED_SHORT;
+        const offset = 0;
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    }
 }
