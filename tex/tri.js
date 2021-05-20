544638eaefbf9e62b3e98e46310cfbd6db2197d8
iirthw <iirthw@gmail.com>
2021-05-20 21:23:50 +0300

    [tex/tri] refactor to initBuffers

--- tex/tri.js
+++ tex/tri.js
@@ -10,17 +10,22 @@ window.onload = function init()
         alert("WebGL isn't available");
     }
 
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
-
     // Configure WebGL
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
-
 
     // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader")
     gl.useProgram(program);
 
+    initBuffers(gl, program);
+
+    render();
+};
+
+function initBuffers(gl, program)
+{
+    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
     // Load the data into the GPU
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
@@ -30,8 +35,7 @@ window.onload = function init()
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
-    render();
-};
+}
 
 function render()
 {
