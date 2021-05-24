50113f621119b3a6e79563657edbf70c8c010f6a
iirthw <iirthw@gmail.com>
2021-05-24 13:33:39 +0300

    [tex/tri] [style] missing semicolon

--- tex/tri.js
+++ tex/tri.js
@@ -15,7 +15,7 @@ window.onload = function init()
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     // Load shaders and initialize attribute buffers
-    const program = initShaders(gl, "vertex-shader", "fragment-shader")
+    const program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
     const buffers = initBuffers(gl, program);
