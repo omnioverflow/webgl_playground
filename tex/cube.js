bf2bb0c6b36defde13dffbdf04da2c42a241d126
iirthw <iirthw@gmail.com>
2021-05-31 23:55:23 +0300

    [tex/cube] WIP on cube rotation

--- tex/cube.js
+++ tex/cube.js
@@ -29,12 +29,31 @@ window.onload = function init()
         }
     };
 
-    const buffers = initBuffers(gl, programInfo);
+    const buffers = initBuffers(gl, programInfo);    
+
+    const model_view = {
+        cube_rotation : 0.0
+    };
+
+    let then = 0;
+    function render(now) {
+        // convert millis to seconds
+        now *= 0.001;
+        const delta_time = now - then;
+        then = now;
+
+        drawScene(gl, programInfo, buffers, 
+            texture, model_view, delta_time);
+
+        requestAnimationFrame(render);
+    }
+
     // FIXME: leave only the url which is in use
     const texUrl = 'https://www.babylonjs-playground.com/textures/bloc.jpg';
-    const texture = loadTexture(gl, texUrl, drawScene, programInfo, buffers);
-
-    drawScene(gl, programInfo, buffers, texture);
+    const texture = loadTexture(gl, texUrl, render, programInfo,
+                                buffers, model_view);
+    
+    requestAnimationFrame(render);
 };
 
 function initBuffers(gl, programInfo)
@@ -75,7 +94,8 @@ function initBuffers(gl, programInfo)
     }
 }
 
-function drawScene(gl, programInfo, buffers, texture)
+function drawScene(gl, programInfo, buffers,
+                   texture, model_view, delta_time)
 {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
@@ -85,20 +105,53 @@ function drawScene(gl, programInfo, buffers, texture)
     // Clear color and depth buffers
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Create a perspective matrix, a special matrix that is
-    // used to simulate the distortion of perspective in a camera.
-    // Our field of view is 45 degrees, with a width/height
-    // ratio that matches the display size of the canvas
-    // and we only want to see objects between 0.1 units
-    // and 100 units away from the camera.
+    {
+        // Create a perspective matrix, a special matrix that is
+        // used to simulate the distortion of perspective in a camera.
+        // Our field of view is 45 degrees, with a width/height
+        // ratio that matches the display size of the canvas
+        // and we only want to see objects between 0.1 units
+        // and 100 units away from the camera.
 
-    // vertical field-of-view
-    const fovy = 45 * Math.PI / 180;   // in radians
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const z_near = 0.1;
-    const z_far = 100.0;
+        // vertical field-of-view
+        const fovy = 45 * Math.PI / 180;   // in radians
+        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        const z_near = 0.1;
+        const z_far = 100.0;
 
-    const projection_mat = mat4.perspective(fovy, aspect, z_near, z_far);
+        const projection_mat = mat4.perspective(fovy, aspect,
+                                                z_near, z_far);
+
+        // Create a model-view matrix
+        let modelview_mat = mat4.create();
+        mat4.translate(modelview_mat, 
+                       [-0.0, 0.0, -6.0],
+                       modelview_mat);
+        // Specify rotation around z-axis
+        const rot_z_radian = model_view.cube_rotation;
+        mat4.rotate(modelview_mat,
+                    rot_z_radian,
+                    [0, 0, 1],
+                    modelview_mat);
+        // Rotation around x-axis
+        const rot_x_radian = .7 * model_view.cube_rotation;
+        mat4.rotate(modelview_mat,
+                    rot_x_radian,
+                    [1, 0, 0],
+                    modelview_mat);
+                    
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.projectionMatrix,
+            false,
+            projection_mat);
+
+        gl.uniformMatrix4fv(
+            programInfo.uniformLocations.modelViewMatrix,
+            false,
+            modelview_mat);
+
+        model_view.cube_rotation += delta_time;
+    }
 
     // Tell GPU how to pull out vertex coordinates
     // from the data associated to vertex attributes
