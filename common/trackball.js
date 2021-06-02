bead86a2fe4f0b8f119de9ed18d4092fd0501ae8
iirthw <iirthw@gmail.com>
2021-06-02 23:31:32 +0300

    [trackball] adding prototype

---
+++ common/trackball.js
@@ -0,0 +1,19 @@
+class Trackball {
+    prevPos = vec2(NaN, NaN);
+    currPos = vec2(NaN, NaN);
+    screenWidth = -1;
+    screenHeight = -1;
+
+    constructor(screenWidth, screenHeight) {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+    }
+
+    onMouseDown(event) {
+        // FIXME: missing impl
+    }
+
+    onMouseUp(event) {
+        // FIXME: missing impl
+    }
+} // class Trackball
