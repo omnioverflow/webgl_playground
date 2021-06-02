1f2ee64c354162931ebb13bd0e17b400d9208505
iirthw <iirthw@gmail.com>
2021-06-02 22:42:11 +0300

    [procedural] refactor cube into a simple class

--- procedural/procedural_cube.js
+++ procedural/procedural_cube.js
@@ -1,30 +1,125 @@
 // window.onload = init();
 
 // function init() {
-//     procedural_cube([0.0, 0.0, 0.0], 1.0);
+//     ProceduralCube([0.0, 0.0, 0.0], 1.0);
 // }
 
-function match_tex_coords(curr_vert,
-                          curr_tex_coord,
-                          tex_coord_cache,
-                          vert_cache) {
-    // No vertices with the same coordinates
-    if (vert_cache == null || !vert_cache.length)
-    // Return itself
-        return curr_vert;
+class ProceduralCube {
 
-    for (let i = 0; i < tex_coord_cache.length; i++) {
-        if (curr_tex_coord[0] == tex_coord_cache[i][0]
-            && curr_tex_coord[1] == tex_coord_cache[i][1])
-            return vert_cache[i];
+    vertexCoordinates = new Float32Array([]);
+    textureCoordinates = new Float32Array([]);
+    faces = new Int16Array([]);
+
+    constructor() {
+        const halfEdge = size / 2;
+        const numVertices = 8;
+        const numCoordinates = 3 * numVertices; 
+        const numFaces = 6;
+
+        let vertices = [];
+    
+        let vertIndex = 0;
+        for (let i = -1; i <= 1; i +=2)
+            for (let j = -1; j <= 1; j += 2)
+                for (let k = -1; k <= 1; k += 2) {
+                    vertices[3 * vertIndex] = i * halfEdge + center[0];
+                    vertices[3 * vertIndex + 1] = j * halfEdge + center[1];
+                    vertices[3 * vertIndex + 2] = k * halfEdge + center[2];
+                    vertIndex++;
+                 }
+
+        let indices = new Uint16Array([
+            // face 0
+            0, 2, 1,
+            1, 2, 3,
+            // face 1
+            0, 1, 4,
+            1, 5, 4,
+            // face 2
+            0, 4, 2,
+            4, 6, 2,
+            // face 3
+            3, 1, 5,
+            3, 5, 7,
+            // face 4
+            6, 3, 2,
+            6, 7, 3,
+            // face 5
+            4, 5, 6,
+            5, 7, 6
+        ]);
+
+        let textureCoords = new Float32Array([
+        // face 0
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 0.0,
+        0.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+        // face 1
+        0.0, 1.0,
+        0.0, 0.0,
+        1.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        // face 2
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+        // face 3
+        1.0, 1.0,
+        1.0, 0.0,
+        0.0, 0.0,
+        1.0, 1.0,
+        0.0, 0.0, 
+        0.0, 1.0,
+        // face 4
+        1.0, 0.0,
+        0.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        1.0, 0.0,
+        // face 5
+        0.0, 0.0,
+        1.0, 0.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        1.0, 1.0, 
+        0.0, 1.0
+        ]);
+
+        const reindexed = reindex_vertex_indices(vertices,
+                                                 indices,
+                                                 textureCoords);
+
+        vertexCoordinates = new Float32Array(reindexed["vertices"]);
+        textureCoordinates = textureCoords;
+        faces = reindexed["indices"];
     }
 
-    return null;
-}
+    match_tex_coords(curr_vert, curr_tex_coord,
+                     tex_coord_cache, vert_cache) {
+        // No vertices with the same coordinates
+        if (vert_cache == null || !vert_cache.length)
+            // Return itself
+            return curr_vert;
 
-function reindex_vertex_indices(vertices, 
-                                indices,
-                                tex_coordinates) {
+        for (let i = 0; i < tex_coord_cache.length; i++) {
+            if (curr_tex_coord[0] == tex_coord_cache[i][0]
+                && curr_tex_coord[1] == tex_coord_cache[i][1])
+                return vert_cache[i];
+        }
+
+        return null;
+    }
+
+    reindex_vertex_indices(vertices, indices, tex_coordinates) {
     let tex_coord_cache = [];
     let vert_cache = [];
 
@@ -84,100 +179,5 @@ function reindex_vertex_indices(vertices,
         "vertices": vertices,
         "indices": indices
     }
-}
-
-function procedural_cube(center, size) {
-    const halfEdge = size / 2;
-    const numVertices = 8;
-    const numCoordinates = 3 * numVertices; 
-    const numFaces = 6;
-
-    let vertices = [];
-    
-    let vertIndex = 0;
-    for (let i = -1; i <= 1; i +=2)
-        for (let j = -1; j <= 1; j += 2)
-            for (let k = -1; k <= 1; k += 2) {
-                vertices[3 * vertIndex] = i * halfEdge + center[0];
-                vertices[3 * vertIndex + 1] = j * halfEdge + center[1];
-                vertices[3 * vertIndex + 2] = k * halfEdge + center[2];
-                vertIndex++;
-            }
-
-    let indices = new Uint16Array([
-            // face 0
-            0, 2, 1,
-            1, 2, 3,
-            // face 1
-            0, 1, 4,
-            1, 5, 4,
-            // face 2
-            0, 4, 2,
-            4, 6, 2,
-            // face 3
-            3, 1, 5,
-            3, 5, 7,
-            // face 4
-            6, 3, 2,
-            6, 7, 3,
-            // face 5
-            4, 5, 6,
-            5, 7, 6
-        ]);
-
-
-    let textureCoords = new Float32Array([
-        // face 0
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 0.0,
-        0.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // face 1
-        0.0, 1.0,
-        0.0, 0.0,
-        1.0, 1.0,
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        // face 2
-        0.0, 0.0,
-        1.0, 0.0,
-        0.0, 1.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        0.0, 1.0,
-        // face 3
-        1.0, 1.0,
-        1.0, 0.0,
-        0.0, 0.0,
-        1.0, 1.0,
-        0.0, 0.0, 
-        0.0, 1.0,
-        // face 4
-        1.0, 0.0,
-        0.0, 1.0,
-        0.0, 0.0,
-        1.0, 0.0,
-        1.0, 1.0,
-        1.0, 0.0,
-        // face 5
-        0.0, 0.0,
-        1.0, 0.0,
-        0.0, 1.0,
-        1.0, 0.0,
-        1.0, 1.0, 
-        0.0, 1.0
-        ]);
-
-    const reindexed = reindex_vertex_indices(vertices,
-                                             indices,
-                                             textureCoords);
-
-    return {
-        vertexCoordinates: new Float32Array(reindexed["vertices"]),
-        textureCoordinates: textureCoords,
-        faces: reindexed["indices"]
     }
-}
+} // class ProceduralCube
