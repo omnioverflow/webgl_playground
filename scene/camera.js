aab1bb278b89feef89c825a02e824741c840d591
iirthw <iirthw@gmail.com>
2021-06-17 22:59:19 +0300

    [camera] [style] minor fix: method ending

--- scene/camera.js
+++ scene/camera.js
@@ -12,5 +12,5 @@ class Camera {
         this.#position = vec3.create(new Float32Array([0.0, 0.0, 0.0]));
         this.#rotationCenter = vec3.create(new Float32Array([0.0, 0.0, 0.0]));
         this.#viewMatrix = mat4.identity();
-    }
+    } // ctor
 };
