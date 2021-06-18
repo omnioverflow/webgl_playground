91bd5172f668ed0109500935a6a32874e1d4b6cc
iirthw <iirthw@gmail.com>
2021-06-18 23:01:13 +0300

    [camera] wip on lookAt

--- scene/camera.js
+++ scene/camera.js
@@ -14,8 +14,60 @@ class Camera {
         this.#viewMatrix = mat4.identity();
     } // ctor
 
-    lookAt(point) {
-        // FIXME: provide impl
+    lookAt(to, translation) {
+        // When computing a view matrix of the camera, we
+        // use the following notation for the axes 
+        // defining the camera position in space:
+        // right (for x-axis), up (for the y-axis),
+        // forward (for z-axis).
+        // Given the right, up, and forward unit vectors,
+        // the view matrix will be composed as:
+        //  [right_x, up_x, forward_x, T_x],
+        //  [right_y, up_y, forward_y, T_y],
+        //  [right_z, up_z, forward_z, T_z],
+        //  [0,       0,    0,         1],
+        // where T is the camera translation.
+
+        // 1. Compute the forward axis (y-axis).
+        const forward = vec3.normalize(vec3.subtract(to - position));
+
+        {
+            // FIXME: handle the edge case when forward is aligned with the y-axis
+            // of the world coordinate system
+            const yAxis = vec3.create([0.0, 1.0, 0.0]);
+            const yAxisNeg = vec3.create([0.0, -1.0, 0.0]);
+            if (forward == yAxis || forward == yAxisNeg) 
+                throw "[Camera] Can't compute forward axis";
+        }
+
+        // 2. Compute the right axis (x-axis)
+        const tmpVector = vec3.create([0.0, 1.0, 0.0]);
+        // Intentionally keep the normalization
+        const right = vec3.cross(vec3.normalize(tmpVector), forward);
+
+        // 3. Compute the up vector
+        const up = vec3.cross(forward, right);
+
+        // 4. Finally, construct the view matrix for the camera
+        let viewMat = mat4.create();
+        viweMat[0] = right[0];
+        viewMat[1] = up[0];
+        viewMat[2] = forward[0];
+        viewMat[3] = translation[0];
+        viewMat[4] = right[1];
+        viewMat[5] = up[1];
+        viewMat[6] = forward[1];
+        viewMat[7] = translation[1];
+        viewMat[8] = right[2];
+        viewMat[9] = up[2];
+        viewMat[10] = forward[2];
+        viewMat[11] = translation[2];
+        viewMat[12] = 0.0;
+        viewMat[13] = 0.0;
+        viewMat[14] = 0.0;
+        viewMat[15] = 1.0;
+
+        this.#viewMatrix = viewMat;
     } // lookAt
 
     moveTo(position) {
