57bf7082fa9d3515045e78413c9f865b0ac060cd
iirthw <iirthw@gmail.com>
2021-07-12 22:39:06 +0300

    [camera] delted lookAtNaive, use lookAt instead

--- scene/camera.js
+++ scene/camera.js
@@ -68,7 +68,7 @@ class Camera {
                 break;
         }
 
-        this.lookAtNaive(position, target, up);
+        this.lookAt(position, target, up);
     } // ctor
 
     get position() {
@@ -158,9 +158,9 @@ class Camera {
         newPosition = vec3.normalize(newPosition);
         newPosition = vec3.scale(newPosition, eyeToTargetDistance, newPosition);
 
-        this.lookAtNaive(newPosition,
-                        this.#target,
-                        vec3.create(new Float32Array([0, 1, 0])));
+        this.lookAt(newPosition,
+                   this.#target,
+                   vec3.create(new Float32Array([0, 1, 0])));
     } // rotateAroundPivot
 
     lookAt(to) {
@@ -170,7 +170,7 @@ class Camera {
 
 
     // Unoptimized implementation of camera "lookAt" method
-    lookAtNaive(eye, target, up) {
+    lookAt(eye, target, up) {
         this.#position = eye;
         this.#target = target;
 
@@ -228,63 +228,6 @@ class Camera {
         // camera matrix)
         // orientation = mat4.transpose(orientation);
         viewMat = mat4.multiply(orientation, translation, viewMat);
-
-        this.setViewMatrix(viewMat);
-    } // lookAtNaive
-
-    lookAt(to, fromPosition) {
-        // When computing a view matrix of the camera, we
-        // use the following notation for the axes 
-        // defining the camera position in space:
-        // right (for x-axis), up (for the y-axis),
-        // forward (for z-axis).
-        // Given the right, up, and forward unit vectors,
-        // the view matrix will be composed as:
-        //  [right_x, up_x, forward_x, T_x],
-        //  [right_y, up_y, forward_y, T_y],
-        //  [right_z, up_z, forward_z, T_z],
-        //  [0,       0,    0,         1],
-        // where T is the camera translation.
-
-        // 1. Compute the forward axis (y-axis).
-        let forward = vec3.create();
-        forward = vec3.normalize(vec3.subtract(to, fromPosition, forward));
-
-        {
-            // FIXME: handle the edge case when forward is aligned with the y-axis
-            // of the world coordinate system
-            const yAxis = vec3.create([0.0, 1.0, 0.0]);
-            const yAxisNeg = vec3.create([0.0, -1.0, 0.0]);
-            if (forward == yAxis || forward == yAxisNeg) 
-                throw "[Camera] Can't compute forward axis";
-        }
-
-        // 2. Compute the right axis (x-axis)
-        const tmpVector = vec3.create([0.0, 1.0, 0.0]);
-        // Intentionally keep the normalization
-        const right = vec3.cross(vec3.normalize(tmpVector), forward);
-
-        // 3. Compute the up vector
-        const up = vec3.cross(forward, right);
-
-        // 4. Finally, construct the view matrix for the camera
-        let viewMat = mat4.create();
-        viewMat[0] = right[0];
-        viewMat[1] = up[0];
-        viewMat[2] = forward[0];
-        viewMat[3] = fromPosition[0];
-        viewMat[4] = right[1];
-        viewMat[5] = up[1];
-        viewMat[6] = forward[1];
-        viewMat[7] = fromPosition[1];
-        viewMat[8] = right[2];
-        viewMat[9] = up[2];
-        viewMat[10] = forward[2];
-        viewMat[11] = fromPosition[2];
-        viewMat[12] = 0.0;
-        viewMat[13] = 0.0;
-        viewMat[14] = 0.0;
-        viewMat[15] = 1.0;
 
         this.setViewMatrix(viewMat);
     } // lookAt
