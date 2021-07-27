448cf05e0a2715e6788e4345335a8e7c50793727
iirthw <iirthw@gmail.com>
2021-07-27 23:57:34 +0300

    [engine.loader] wip on engine loader (part 2)

--- engine/engine.loader.js
+++ engine/engine.loader.js
@@ -1,38 +1,60 @@
-//
-// Dynamically load a script
-// (in spirit of: 
-// https://stackoverflow.com/questions/14521108/dynamically-load-js-inside-js
-// and
-// https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file)
-// and
-// https://unixpapa.com/js/dyna.html).
-// 
-// url - external script to load;
-// domLocation - is the location to insert <script> element to (e.g. head, body)
-function dynamicallyLoadScript(url, domLocation) {
-    // Create a script DOM node
-    var script = document.createElement('script');
-    // Set its src to the provided URL
-    script.src = url;
+class EngineLoader {
 
-    domLocation.appendChild(script);
-}
+    //
+    // Dynamically load a script
+    // (in spirit of: 
+    // https://stackoverflow.com/questions/14521108/dynamically-load-js-inside-js
+    // and
+    // https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file)
+    // and
+    // https://unixpapa.com/js/dyna.html).
+    // 
+    // url - external script to load;
+    // domLocation - is the location to insert <script> element to (e.g. head, body)
+    static dynamicallyLoadScript(url, domLocation, totalNumber, loaded, finalCallback) {
+        // Create a script DOM node
+        var script = document.createElement('script');
+        // Set its src to the provided URL
+        script.src = url;
+        script.setAttribute("defer", "defer");
 
-function engineLoader() {
-    dynamicallyLoadScript("../engine/camera/Camera.js", document.body);
-    dynamicallyLoadScript("../engine/controls/Trackball.js", document.body);
-    dynamicallyLoadScript("../engine/data/teapot.js", document.body);
-    dynamicallyLoadScript("../engine/loader/ObjLoader.js", document.body);
-    dynamicallyLoadScript("../engine/loader/TexLoader.js", document.body);
-    dynamicallyLoadScript("../engine/math/Matrix.js", document.body);
-    dynamicallyLoadScript("../engine/math/MathCommon.js", document.body);
-    dynamicallyLoadScript("../engine/procedural/Procedural.js", document.body);
-    dynamicallyLoadScript("../engine/scene/Scene.js", document.body);
-    dynamicallyLoadScript("../engine/shader/InitShaders.js", document.body);
-    dynamicallyLoadScript("../engine/utils/WebGLUtils.js", document.body);
-} // engineLoader
+        domLocation.appendChild(script);
 
-// Bind loader script to onload event of the window in which
-// the script is running (for the lack of better knowledge of 
-// how to do that  in other way).
-window.addEventListener("load", engineLoader);
+        script.onload = () => {
+            loaded.number = loaded.number + 1;
+            if (totalNumber == loaded.number && finalCallback) {
+                finalCallback();
+            }
+        };
+    } // dynamicallyLoadScript
+
+    static loadEngine(finalCallback) {
+        let scripts = [
+            '../engine/camera/Camera.js',
+            '../engine/controls/Trackball.js',
+            '../engine/data/teapot.js',
+            '../engine/loader/ObjLoader.js',
+            '../engine/loader/TexLoader.js',
+            '../engine/math/Matrix.js',
+            '../engine/math/MathCommon.js',
+            '../engine/procedural/Procedural.js',
+            '../engine/scene/Scene.js',
+            '../engine/shader/InitShaders.js',
+            '../engine/utils/WebGLUtils.js'
+        ];
+
+        const nbScripts = scripts.length;
+        let nbLoaded = {};
+        nbLoaded.number = 0;
+        for (const script of scripts) {
+            EngineLoader.dynamicallyLoadScript(script, document.body,
+                nbScripts, nbLoaded, finalCallback);   
+        }
+    } // loadEngine
+
+    // Bind loader script to onload event of the window in which
+    // the script is running (for the lack of better knowledge of 
+    // how to do that  in other way).
+    // window.onload = engineLoader();
+
+} // EngineLoader
