a8cea259edcbaa68d946ffc57f57f4e4aed20038
iirthw <iirthw@gmail.com>
2021-08-07 23:17:17 +0300

    Add a stub for effects/ScreenOverlay

---
+++ engine/scene/effects/ScreenOverlay.js
@@ -0,0 +1,12 @@
+// TODO: provide impl.
+
+/*
+	A class that implements a renderable overlay (full sqreen quad)
+	that fully covers the sqreen space.
+
+	One of the main usages is to make it possible to implement scree-space effects.
+*/
+
+class ScreenOverlay {
+	
+} // class ScreenOverlay
