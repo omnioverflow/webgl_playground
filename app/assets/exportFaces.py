32d091b141c8bdd86d97a365fa3328cbebb9347b
iirthw <iirthw@gmail.com>
2021-08-12 23:31:07 +0300

    [py] export obj -> js (substitute multiple spaces with a single one, remove leading and trailing spaces)

--- app/assets/exportFaces.py
+++ app/assets/exportFaces.py
@@ -5,6 +5,10 @@ def exportFaces(fileName):
     faces = []
     with open(fileName) as f:        
         for line in f:
+            # As in case of exporting vertices, substitute multiple spaces
+            # with a single space and remove leading and trailing spaces.
+            # (poor solution since we do two consecutive splits).
+            line = ' '.join(line.split())
             line = re.split(' |\n', line)
             if line[0] == 'f':
                 line = line[1:4]
--- app/assets/exportVertices.py
+++ app/assets/exportVertices.py
@@ -5,6 +5,11 @@ def exportVertices(fileName):
     vertices = []
     with open(fileName) as f:        
         for line in f:
+            # Substitute multiple spaces by a single one,
+            # and remove leading and trailing spaces.
+            # (it is kind of waste of computational resources to do two 
+            # consequtive splits, though).
+            line = ' '.join(line.split())
             line = re.split(' |\n', line)
             if line[0] == 'v':
                 line = line[1:4]
