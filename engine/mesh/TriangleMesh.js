7999b3cb14a8f1edb8f0f0c12392b5888a0d9767
iirthw <iirthw@gmail.com>
2021-08-13 13:36:21 +0300

    [engine] (memory footprint  impact) TriangleMesh pod (data copy)

--- engine/mesh/TriangleMesh.js
+++ engine/mesh/TriangleMesh.js
@@ -1,10 +1,22 @@
-// TODO: provide impl
 
+/**
+ * A wrapper representing a triangular mesh.
+ * 
+ * TODO: what is important is to make sure that a TriangleMesh object does not
+ * create a copy of vertexPositions and triangles,
+ * but simply reference the input objects in ctor.
+ */
 class TriangleMesh {
+    //--------------------------------------------------------------------------
     #vertexPositions
     #triangles
+    //--------------------------------------------------------------------------
 
-    static createFromOBJ(filename) {
-        // TODO: provide impl
-    } // createFromOBJ
+    constructor(vertexPositions, triangles) {
+        // FIXME: does it make a copy or just a reference to the input objects?
+        this.#vertexPositions = vertexPositions;
+        this.#triangles = triangles;
+    }
+
+    //--------------------------------------------------------------------------
 } // class TriangleMesh
