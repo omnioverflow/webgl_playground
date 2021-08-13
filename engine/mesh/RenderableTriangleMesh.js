d4fc71b92c0f749efbe639d99faead73e7ea2bbe
iirthw <iirthw@gmail.com>
2021-08-13 13:37:40 +0300

    [engine] RenderableTriangleMesh (first iter)

--- engine/mesh/RenderableTriangleMesh.js
+++ engine/mesh/RenderableTriangleMesh.js
@@ -1,4 +1,26 @@
 // TODO: provide impl
 
 class RenderableTriangleMesh {
+    //--------------------------------------------------------------------------
+    #triangleMesh
+    //--------------------------------------------------------------------------
+    constructor(triangleMesh, lazyLoad = false) {
+        this.#triangleMesh = triangleMesh;
+
+        if (!lazyLoad) {
+            this.bindBuffers();
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    
+    bindBuffers() {
+        // TODO: impl
+    }
+
+    //--------------------------------------------------------------------------
+    get triangleMesh() {
+        return this.#triangleMesh;
+    }
+    //--------------------------------------------------------------------------
 } // class RenderableTriangleMesh
