d23556c770818a857136f5399623a992863e7295
iirthw <iirthw@gmail.com>
2021-10-23 22:10:10 +0300

    [mnist] add rgbToHex conversion

--- app/mnist-digits/index.js
+++ app/mnist-digits/index.js
@@ -14,6 +14,15 @@ var clickY = [];
 var clickDrag = [];
 var paint;
 
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 /**
  * Add information where the user clicked at.
  * @param {number} x
